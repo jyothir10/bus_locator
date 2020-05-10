5f6c8fab2ac225ed873f360ccd4ab4de1ada849d
Muhammed Shameem <toshameem2001@gmail.com>
2020-05-10 16:34:57 +0400

    login

---
+++ lib/homePage.dart
@@ -0,0 +1,28 @@
+import 'package:bus_locator/Authentication/ui/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class HomePage extends StatelessWidget {
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
+  void _signOut() async {
+    await googleSignIn.signOut();
+  }
+
+  static String id = 'Home_Screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+            child: Center(
+          child: GestureDetector(
+              onTap: () async {
+                _signOut();
+                await Navigator.pushReplacementNamed(context, LoginPage.id);
+              },
+              child: Container(child: Text('Home'))),
+        )),
+      ),
+    );
+  }
+}
