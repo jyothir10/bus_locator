b868e2fae2c2a61b8a43cc7753f2fc66da3f9469
Muhammed Shameem <toshameem2001@gmail.com>
2020-05-10 18:51:46 +0400

    login

--- lib/Authentication/ui/login_page.dart
+++ lib/Authentication/ui/login_page.dart
@@ -6,7 +6,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bus_locator/Authentication/style/theme.dart' as Theme;
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+
 
 class LoginPage extends StatefulWidget {
   static String id = 'Login_Screen';
