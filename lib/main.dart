b6cd1055c15b1ad098c9256cbb1e092dfc825a9e
jyothir10 <j.adithyan16@gmail.com>
2020-05-11 15:27:39 +0530

    ha

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Destination(),
+      home: LoginPage(),
     );
   }
 }
