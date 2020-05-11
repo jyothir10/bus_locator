2f263cd4732e54c132789b94288373eb2053777a
jyothir10 <j.adithyan16@gmail.com>
2020-05-11 23:08:48 +0530

    ha

---
+++ lib/Components/buscard.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'reusableCard.dart';
+
+class BusCard extends StatelessWidget {
+  final String busName;
+  final String distance;
+  final double fare;
+  final String busType;
+
+  BusCard({this.busName, this.distance, this.busType, this.fare});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: ReusableCard(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  bottomLeft: Radius.circular(8.0)),
+              cardChild: Padding(
+                padding: const EdgeInsets.all(19.0),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.directions_bus,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          busName,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          busType,
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          distance,
+                          style: TextStyle(
+                              color: Colors.red, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              colour: Color(0xFF28284D),
+              onPress: () {},
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              cardChild: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  height: 65.0,
+                  child: Center(
+                    child: Text(
+                      fare.toString(),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0),
+                    ),
+                  ),
+                ),
+              ),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8.0),
+                bottomRight: Radius.circular(8.0),
+              ),
+              colour: Color(0xFF242448),
+              onPress: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
