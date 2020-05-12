251e5ae9f13577745cbc735a688a8cdaf82077fb
sanika2001 <porayilsanika@gmail.com>
2020-05-12 23:48:50 +0530

    0

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CartScreen(),
-//      home: AnimatedSplashScreen(),
+      home: AnimatedSplashScreen(),
     );
   }
 }
