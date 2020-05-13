795301e7b775c55301e5f246678a5a194e4a9850
sanika2001 <porayilsanika@gmail.com>
2020-05-14 00:17:45 +0530

    0

--- lib/screens/Tutorial SCreen.dart
+++ lib/screens/Tutorial SCreen.dart
@@ -3,7 +3,7 @@ import 'package:bus_locator/Components/RoundedButton.dart';
 import 'package:bus_locator/Components/TabBar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:dots_indicator/dots_indicator.dart';
 
 class TutorialScreen extends StatefulWidget {
   static String id = 'Tutorial_Screen';
@@ -42,14 +42,14 @@ class _TutorialScreenState extends State<TutorialScreen> {
             Column(
               children: <Widget>[
                 Container(
-                  width: 210.0,
-                  height: 210.0,
+                  width: 250.0,
+                  height: 250.0,
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     image: new DecorationImage(
                       fit: BoxFit.fill,
                       image: AssetImage(
-                        'assets/map purple.png',
+                        'assets/purple map.jpg',
                       ),
                     ),
                   ),
@@ -60,6 +60,16 @@ class _TutorialScreenState extends State<TutorialScreen> {
               child: Text(
                 tutorial,
                 style: TextStyle(color: Colors.grey),
+              ),
+            ),
+            Center(
+              child: DotsIndicator(
+                dotsCount: 3,
+                position: 1,
+                decorator: DotsDecorator(
+                  color: Colors.white,
+                  activeColor: kBottomBarActiveIconColor,
+                ),
               ),
             ),
             Row(
