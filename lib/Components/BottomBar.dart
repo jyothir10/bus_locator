397a44963a34b24ef922a5f6c8da9dbf1b19dc5d
Muhammed Shameem <toshameem2001@gmail.com>
2020-05-13 04:18:59 +0400

    splash screen and other minor changes

--- lib/Components/BottomBar.dart
+++ lib/Components/BottomBar.dart
@@ -1,3 +1,7 @@
+import 'package:bus_locator/homePage.dart';
+import 'package:bus_locator/screens/CartScreen.dart';
+import 'package:bus_locator/screens/cart_profile.dart';
+import 'package:bus_locator/screens/destination_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bus_locator/Components/Constants.dart';
@@ -21,28 +25,41 @@ class _BottomBarState extends State<BottomBar> {
       currentIndex: _currentIndex,
       items: [
         BottomNavigationBarItem(
-          icon: Icon(
-            Icons.home,
-            size: 27,
+          icon: GestureDetector(
+            onTap: () => Navigator.pushReplacementNamed(context, HomePage.id),
+            child: Icon(
+              Icons.home,
+              size: 27,
+            ),
           ),
           title: Text('home'),
           backgroundColor: kBottomBarColor,
         ),
         BottomNavigationBarItem(
-          icon: Icon(
-            FontAwesomeIcons.bus,
-            size: 22,
+          icon: GestureDetector(
+            onTap: () =>
+                Navigator.pushReplacementNamed(context, Destination.id),
+            child: Icon(
+              FontAwesomeIcons.bus,
+              size: 22,
+            ),
           ),
           title: Text('bus'),
           backgroundColor: kBottomBarColor,
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.add_shopping_cart, size: 27),
+          icon: GestureDetector(
+              onTap: () =>
+                  Navigator.pushReplacementNamed(context, CartScreen.id),
+              child: Icon(Icons.add_shopping_cart, size: 27)),
           title: Text('cart'),
           backgroundColor: kBottomBarColor,
         ),
         BottomNavigationBarItem(
-          icon: Icon(FontAwesomeIcons.user, size: 22),
+          icon: GestureDetector(
+              onTap: () =>
+                  Navigator.pushReplacementNamed(context, CartProfile.id),
+              child: Icon(FontAwesomeIcons.user, size: 22)),
           title: Text(
             'account',
           ),
