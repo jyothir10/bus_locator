1c8d45e4a15db55b73fa12aea2da73f457a4b660
jyothir10 <j.adithyan16@gmail.com>
2020-05-14 11:01:19 +0530

    ha

--- lib/screens/orderScreen.dart
+++ lib/screens/orderScreen.dart
@@ -2,6 +2,7 @@ import 'package:bus_locator/Components/reusableCard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:bus_locator/Components/Constants.dart';
+import 'package:bus_locator/Components/RoundedButton.dart';
 
 class OrderScreen extends StatefulWidget {
   static String id = 'Order_Screen';
@@ -27,12 +28,65 @@ class _OrderScreenState extends State<OrderScreen> {
       body: Container(
         color: kAppBarColor,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             BuscardOrderPage(
               busName: 'Bus A01',
               busType: 'Non AC/Hino AKJ1',
               far: '15min Away',
               distance: 'Distance: 7.4 Km',
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'Price',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                maxLines: 1,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Total Price',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    '9.81',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                maxLines: 1,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: RoundedButton(
+                color: kButtonActiveColor,
+                text: 'Continue',
+                onPress: null,
+                textColor: Colors.white,
+              ),
             ),
           ],
         ),
@@ -47,11 +101,11 @@ class BuscardOrderPage extends StatelessWidget {
   final String busType;
   final String far;
 
-  BuscardOrderPage({this.busName, this.busType, this.distance,this.far});
+  BuscardOrderPage({this.busName, this.busType, this.distance, this.far});
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       child: ReusableCard(
         colour: Color(0xFF242448),
         cardChild: Row(
@@ -70,31 +124,38 @@ class BuscardOrderPage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(
-                    busName,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      busName,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
                   ),
-                  Text(
-                    busType,
-                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 3),
+                    child: Text(
+                      busType,
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
                   ),
                   Text(
                     far,
-                    style: TextStyle(
-                        color: Colors.red, fontWeight: FontWeight.bold),
+                    style: TextStyle(color: Colors.red),
                   ),
                 ],
               ),
             ),
-            Text(
-              distance,
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 14,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                distance,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                ),
               ),
             )
           ],
