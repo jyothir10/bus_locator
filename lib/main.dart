a7c4aac6aa070db3eccbb1d20937bff5dc4ec7a7
Muhammed Shameem <toshameem2001@gmail.com>
2020-05-15 14:12:03 +0400

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ResetPassword(),
+      home: AnimatedSplashScreen(),
     );
   }
 }
